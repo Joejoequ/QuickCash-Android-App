a819aaef3ea6d214fb5f4e6a409424f683297886
yuqingc <yq822350@dal.ca>
2021-01-30 15:59:43 +0800

    Initial UI for sign up page

--- app/src/main/java/com/example/project1/SignUpPage.java
+++ app/src/main/java/com/example/project1/SignUpPage.java
@@ -1,4 +1,14 @@
 package com.example.project1;
 
-public class SignUpPage {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_page);
+    }
 }
