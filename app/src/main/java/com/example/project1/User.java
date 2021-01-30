8ba7c0ff509c9f754eab0fec1cec0e32490d2722
yuqingc <yq822350@dal.ca>
2021-01-30 15:12:56 +0800

    set up some test case for UserName and Password restriction

---
+++ app/src/main/java/com/example/project1/User.java
@@ -0,0 +1,55 @@
+package com.example.project1;
+
+public class User {
+    public String userName;
+    public String password;
+
+    public User(){
+
+    }
+    // Initialization of user
+    public User(String name, String password) {
+        this.userName = name;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // User can change their password as they want
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    // User can change their userName as they want
+    public void changeName(String name) {
+        this.userName = name;
+    }
+
+    // Check if enter an empty userName
+    public boolean isEmptyUserName(User user) {
+        return true;
+    }
+
+    // Check if enter an empty password
+    public boolean isEmptyPassword(User user) {
+        return true;
+    }
+
+    // Check if enter a valid username, should be less than 16 digits
+    public boolean isValidUserName(User user) {
+        return true;
+    }
+
+    // Check if enter a valid password, should be no less than 8 digits, no more than 16 letters
+    // The password need to include at least a combination of letters and number
+    public boolean isValidPassword(User user) {
+        return true;
+    }
+
+}
--- app/src/test/java/com/example/project1/ExampleUnitTest.java
+++ app/src/test/java/com/example/project1/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.project1;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +11,61 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    static MainActivity mainActivity;
+    static SignUpPage signUp;
+    static User user;
+
+    @BeforeClass
+    public static void setup(){
+        mainActivity = new MainActivity();
+        signUp = new SignUpPage();
+        user = new User();
+    }
+
+    /**AT3:1-1**/
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void checkIfUserNameIsEmpty() {
+        User temp1 = new User("","abc");
+        assertTrue(user.isEmptyPassword(temp1));
+        User temp2 = new User("abd","def");
+        assertFalse(user.isEmptyPassword(temp2));
+    }
+
+    /**AT3:1-2**/
+    @Test
+    public void checkIfPasswordIsEmpty() {
+        User temp1 = new User("abc","");
+        assertTrue(user.isEmptyPassword(temp1));
+        User temp2 = new User("abd","deff");
+        assertFalse(user.isEmptyPassword(temp2));
+    }
+
+    /**AT3:1-3**/
+    public void checkValidUsername() {
+        User temp1 = new User("and123","1111");
+        assertTrue(user.isValidUserName(temp1));
+        User temp2 = new User("ans@#<", "1111");
+        assertFalse(user.isValidUserName(temp2));
+        // check long UserName
+        User temp3 = new User("aaaaaaaaaaaaaaaaaaaa","1111");
+        assertFalse(user.isValidUserName(temp3));
+    }
+
+    /**AT3:1-4**/
+    public void checkValidPassword() {
+        // check short password
+        User temp1 = new User("abc","1111");
+        assertFalse(user.isEmptyPassword(temp1));
+        // check long password
+        User temp2 = new User("abd","11111111111111111111");
+        assertFalse(user.isValidPassword(temp2));
+        // password only contain letters of number
+        User temp4 = new User("abc","1111111111");
+        User temp5 = new User("abc","aaaaaaaaaa");
+        assertFalse(user.isValidPassword(temp4));
+        assertFalse(user.isValidPassword(temp5));
+        // valid password
+        User temp3 = new User("abd","123adf@#$12");
+        assertTrue(user.isValidPassword(temp3));
     }
 }
