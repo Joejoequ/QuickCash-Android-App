75df554e37ada18f67c4d338403def8e1b835f88
jqu <jn679016@dal.ca>
2021-01-31 20:22:06 +0800

    implementing the login activity

--- app/src/main/java/com/example/project1/LogIn.java
+++ app/src/main/java/com/example/project1/LogIn.java
@@ -1,13 +1,32 @@
 package com.example.project1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LogIn extends AppCompatActivity implements View.OnClickListener {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import com.google.firebase.database.ValueEventListener;
+
+public class LogIn extends AppCompatActivity  {
+
+    private EditText mUserNameEditText, mPasswordEditText;
+    private Button mSigninButton;
+    private Button switchToSignUp;
+
+    private DatabaseReference dbUser;
     //java.util.UUID.randomUUID().toString()
 
     @Override
@@ -15,17 +34,28 @@ public class LogIn extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        // event handle to sign up
-        Button switchToSignUp = findViewById(R.id.toSignUp);
-        switchToSignUp.setOnClickListener(this);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        mUserNameEditText = findViewById(R.id.userName);
+        mPasswordEditText = findViewById(R.id.password);
+        mSigninButton = findViewById(R.id.loginBtn);
+        dbUser = FirebaseDatabase.getInstance().getReference("User");
+        switchToSignUp = findViewById(R.id.toSignUp);
+
+
+        switchToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent switchSignUp = new Intent(LogIn.this, SignUpPage.class);
+                startActivity(switchSignUp);
+            }
+        });
     }
 
-    @Override
-    public void onClick(View view) {
-        // change to sign up page
-        Intent switchSignUp = new Intent(this, SignUpPage.class);
-        startActivity(switchSignUp);
-    }
+
 
     static public boolean connection(){
         return false;
