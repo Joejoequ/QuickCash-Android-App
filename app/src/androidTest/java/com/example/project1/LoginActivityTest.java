63aea44fa5f47b37bac4065c5cd9edcd1c89a2fa
jqu <jn679016@dal.ca>
2021-01-31 17:09:43 +0800

    adding test

--- app/src/androidTest/java/com/example/project1/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/project1/LoginActivityTest.java
@@ -2,14 +2,28 @@ package com.example.project1;
 
 import android.content.Context;
 
+import androidx.test.espresso.ViewAction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 /**
@@ -21,7 +35,7 @@ import static org.junit.Assert.*;
 public class LoginActivityTest {
 
     @Rule
-    public ActivityScenarioRule<LogIn> Login = new ActivityScenarioRule<>(LogIn.class);
+    public ActivityTestRule<LogIn> mActivityTestRule = new ActivityTestRule<>(LogIn.class);
 
     @Test
     public void useAppContext() {
@@ -33,9 +47,51 @@ public class LoginActivityTest {
 
     @Test
     public void EmptyLogin() {
-
+        onView(withId(R.id.loginBtn)).perform(click());
+        onView(withText("enter your username"))
+                .inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
 
     }
+
+    @Test
+    public void EmptyPassword() {
+        onView(withId(R.id.userName)).perform(typeText("admin"), closeSoftKeyboard());
+        onView(withId(R.id.loginBtn)).perform(click());
+        onView(withText("enter your password"))
+                .inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void UsernameNotExist() {
+        onView(withId(R.id.userName)).perform(typeText("texxsa"), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("123456"), closeSoftKeyboard());
+        onView(withId(R.id.loginBtn)).perform(click());
+        onView(withText("Incorrect UserName or Password"))
+                .inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+
+    }
+
+
+
+    @Test
+    public void WrongPassword() {
+        onView(withId(R.id.userName)).perform(typeText("logintest"), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("14234"), closeSoftKeyboard());
+        onView(withId(R.id.loginBtn)).perform(click());
+        onView(withText("enter your password"))
+                .inRoot(withDecorView(not(is(mActivityTestRule.getActivity().getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+
+    }
+
+   
+
+
+
 
 
 
