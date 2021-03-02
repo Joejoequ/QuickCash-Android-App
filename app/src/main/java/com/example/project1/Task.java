d2134bfee5399fbc0d71c2d28761bf24e88ce6c8
yuqingc <yq822350@dal.ca>
2021-03-02 22:56:10 +0800

    add compare feature for Task class

--- app/src/main/java/com/example/project1/Task.java
+++ app/src/main/java/com/example/project1/Task.java
@@ -1,6 +1,7 @@
 package com.example.project1;
 
 import java.text.SimpleDateFormat;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -83,5 +84,18 @@ public class Task {
     public int getWage() {
         return wage;
     }
+
+    public static Comparator<Task> postDateSort = new Comparator<Task>() {
+        @Override
+        public int compare(Task t1, Task t2) {
+
+            // if task 1 is posted before t2
+            if (t1.postDate.compareTo(t2.postDate) > 0) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    };
 
 }
