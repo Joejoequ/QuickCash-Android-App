a6d075a19f961b63d44134aaf247bdc96e4628a0
yuqingc <yq822350@dal.ca>
2021-03-09 19:09:47 +0800

    initial espresso test for home page

---
+++ app/src/androidTest/java/com/example/project1/HomeFragmentTest.java
@@ -0,0 +1,32 @@
+package com.example.project1;
+
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class HomeFragmentTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> myRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void checkLoggedMyPostPage() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withText("Home")).perform(click());
+        onView(withId(R.id.HomeSearchView)).check(matches(isDisplayed()));
+        onView(withId(R.id.HomeListView)).check(matches(isDisplayed()));
+    }
+}
