1d1c2cbd0719ceb4e9260b4aa34bb51e9a1e5bbb
yuqingc <yq822350@dal.ca>
2021-04-02 02:01:31 +0800

    link the chat fragment with side bar

--- app/src/main/java/com/example/project1/ui/chat/ChatFragment.java
+++ app/src/main/java/com/example/project1/ui/chat/ChatFragment.java
@@ -1,6 +1,24 @@
 package com.example.project1.ui.chat;
 
-import android.app.Fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.project1.R;
 
 public class ChatFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                                ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.activity_chat, container, false);
+        return root;
+    }
 }
+
+
