79fd34908850a7fa490c2c3aad67f547c5d7490a
yuqingc <yq822350@dal.ca>
2021-04-05 20:47:01 +0800

    implment the function to store and receive message from firebase

--- app/src/main/java/com/example/project1/ui/chat/Chat.java
+++ app/src/main/java/com/example/project1/ui/chat/Chat.java
@@ -1,17 +1,27 @@
 package com.example.project1.ui.chat;
 
 import android.os.Bundle;
+import android.text.style.IconMarginSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.project1.MainActivity;
 import com.example.project1.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Chat extends AppCompatActivity {
@@ -20,18 +30,27 @@ public class Chat extends AppCompatActivity {
     private Button send;
     private RecyclerView msgRecyclerView;
     private com.example.project1.ui.chat.MsgAdapter adapter;
+    private String receiver;
+    private String sender;
+    private DatabaseReference dbChat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_chat);
+        // the receiver and sender of the message
+        receiver = (String)getIntent().getSerializableExtra("receiver");
+        sender = (String)getIntent().getSerializableExtra("sender");
+        getMessage(sender,receiver);
 
-        initMsgs();
+        //initMsgs();
         inputText = findViewById(R.id.input_text);
         send = findViewById(R.id.send_button);
         msgRecyclerView = findViewById(R.id.msg_chatting_recycler_view);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         msgRecyclerView.setLayoutManager(layoutManager);
+
         adapter = new com.example.project1.ui.chat.MsgAdapter(msgList);
         msgRecyclerView.setAdapter(adapter);
         send.setOnClickListener(new View.OnClickListener() {
@@ -39,12 +58,15 @@ public class Chat extends AppCompatActivity {
             public void onClick(View view) {
                 String content = inputText.getText().toString();
                 if(!content.isEmpty()){
+                    sendMessage(sender, receiver, content);
                     com.example.project1.ui.chat.Msg msg = new com.example.project1.ui.chat.Msg(content, com.example.project1.ui.chat.Msg.TYPE_SENT);
                     msgList.add(msg);
                     adapter.notifyItemInserted(msgList.size() - 1);
                     msgRecyclerView.scrollToPosition(msgList.size() - 1);
-                    inputText.setText("");
+                } else {
+                    Toast.makeText(Chat.this, "Can not send empty message", Toast.LENGTH_LONG).show();
                 }
+                inputText.setText("");
             }
         });
     }
@@ -56,6 +78,45 @@ public class Chat extends AppCompatActivity {
         msgList.add(msg2);
         com.example.project1.ui.chat.Msg msg3 = new com.example.project1.ui.chat.Msg("I'm Tiger.", com.example.project1.ui.chat.Msg.TYPE_RECEIVED);
         msgList.add(msg3);
+    }
+
+    private void sendMessage(String sender, String receiver, String content) {
+        dbChat = FirebaseDatabase.getInstance().getReference();
+        HashMap<String, Object> chatMessage = new HashMap<>();
+        chatMessage.put("sender", sender);
+        chatMessage.put("receiver", receiver);
+        chatMessage.put("message", content);
+        dbChat.child("Message").push().setValue(chatMessage);
+    }
+
+    private void getMessage(String sender, String receiver) {
+        dbChat = FirebaseDatabase.getInstance().getReference("Message");
+        dbChat.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                msgList.clear();
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    String s = (String)dataSnapshot.child("sender").getValue();
+                    String r = (String)dataSnapshot.child("receiver").getValue();
+                    String message = (String)dataSnapshot.child("message").getValue();
+                    if (s.equals(sender) && r.equals(receiver)) {
+                        com.example.project1.ui.chat.Msg msg = new com.example.project1.ui.chat.Msg(message, Msg.TYPE_SENT);
+                        msgList.add(msg);
+                        adapter.notifyDataSetChanged();
+                    } else if ((s.equals(receiver) && r.equals(sender))) {
+                        com.example.project1.ui.chat.Msg msg = new com.example.project1.ui.chat.Msg(message, Msg.TYPE_RECEIVED);
+                        msgList.add(msg);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
 }
